9312a241f367047a3fdab577b1ad210138e2e374
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-10-15 00:13:37 +0300

    Create menu.js

---
+++ system/menu.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    var  menuLink = '<li><a href="https://grcodeclub.github.io/en">Home</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = ' <li><a href="https://grcodeclub.github.io/en/system.html">Operating system</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = '<li><a href="#">Windows</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = '<li><a href="https://grcodeclub.github.io/en/system/linux.html">Linux</li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = '<li><a href="#">Android</li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = '<li><a href="#">File Extensions</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = '<li style="height: 200px;"></li>';
+    $('.sidebarMenuInner').append(menuLink);
+  });
