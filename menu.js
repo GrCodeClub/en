641b7d70cea18e1ebc635ab1463f60ea324f14fe
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-10-15 00:11:18 +0300

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    
+    var menuLink = ' <li><a href="https://grcodeclub.github.io/network/">Network</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io/system/">Operating system</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li><a href="https://grcodeclub.github.io/programming/">Programming</a></li>';
+     $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io/cybersecurity/">Cybersecurity</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li style="height: 200px;"></li>';
+    $('.sidebarMenuInner').append(menuLink);
+  });
+  
