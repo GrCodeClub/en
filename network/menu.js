4f6f4bcfe2b1c354312989e5709c2e6b106c3e37
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-10-15 21:20:44 +0300

    Update menu.js

--- network/menu.js
+++ network/menu.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var menuLink = ' <li><a href="https://grcodeclub.github.io/en/">Home</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     
-    menuLink = '<li><a href="#">Network</a></li>';
+    menuLink = '<li><a href="https://grcodeclub.github.io/en/network.html">Network</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
     menuLink = '<li><a href="https://grcodeclub.github.io/en/network/protocol.html">Protocols</a></li>';
