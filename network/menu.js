584a13649df2305f1f669c36a699908a1a44c3ab
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-10-15 21:16:48 +0300

    Create menu.js

---
+++ network/menu.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    
+    var menuLink = ' <li><a href="https://grcodeclub.github.io/en/">Home</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="#">Network</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li><a href="https://grcodeclub.github.io/en/network/protocol.html">Protocols</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li><a href="https://grcodeclub.github.io/network/">Greek</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li style="height: 200px;"></li>';
+    $('.sidebarMenuInner').append(menuLink);
+  });
+  
